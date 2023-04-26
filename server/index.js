f1a417731b1d43249f575fdddcf24aebabfac906
Millenmark <millenmark.iv@gmail.com>
2023-04-26 15:04:00 +0800

    check if the server is working

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
-console.log('hello');
+const express = require('express');
+const app = express();
+
+const port = 3001;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const server = app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+// Check if server is already running
+if (server.listening) {
+  console.log(`Server is already running on port ${port}`);
+} else {
+  console.log(`Server is not running`);
+}
