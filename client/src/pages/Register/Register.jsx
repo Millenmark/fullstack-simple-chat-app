52f26a6d4a3a2a8c65f3c21610739c3306d89da1
Millenmark <millenmark.iv@gmail.com>
2023-04-27 15:59:47 +0800

    added form for registration

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,6 +1,46 @@
+import style from './Register.module.css'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className={style.formContainer}>
+      <form className={style.form}>
+        <h2>Register</h2>
+        <div className={style.formWrapper}>
+          <div className="form-field">
+            <label htmlFor="username">Username</label>
+            <input 
+              type="text" 
+              name="username" 
+              id="username"
+              placeholder="Enter your Username" 
+            />
+          </div>
+
+          <div className="form-field">
+            <label htmlFor="email">Email</label>
+            <input 
+              type="email" 
+              name="email" 
+              id="email"
+              placeholder="Enter your email" 
+            />
+          </div>
+
+          <div className="form-field">
+            <label htmlFor="password">Password</label>
+            <input 
+              type="password" 
+              name="password" 
+              id="password"
+              placeholder="Enter your password" 
+            />
+          </div>
+        </div>
+        
+      </form>
+    </div>
   )
 }
 
