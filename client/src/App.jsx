377653099321255ad7a9692189e6f3a96057f3d9
Millenmark <millenmark.iv@gmail.com>
2023-04-27 14:30:47 +0800

    added css reset

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,19 @@
 import './App.css'
 import {Routes, Route, Navigate} from 'react-router-dom'
 import { Chat, Login, Register } from './pages'
+import { Container } from './UI'
 
 function App() {
 
   return (
-    <>
+    <Container>
       <Routes>
         <Route path='/' element={<Chat/>}/>
         <Route path='/register' element={<Register/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='*' element={<Navigate to='/'/>}/>
       </Routes>
-    </>
+    </Container>
   )
 }
 
