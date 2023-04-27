bf6c69d7d1bf0e7f1655af29d43fc5e551b1f148
Millenmark <millenmark.iv@gmail.com>
2023-04-27 14:27:28 +0800

    added ui folder with container.jsx

---
+++ client/src/UI/Container/Container.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types'
+import style from './Container.module.css'
+
+const Container = ({children, className}) => {
+  return (
+    <div className={`${style.container} ${className}`}>{children}</div>
+  )
+}
+
+Container.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string
+}
+
+export default Container
