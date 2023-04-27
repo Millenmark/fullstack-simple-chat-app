ce7ddd8f14cb6d027491117ff2de5ffe7b3dd5a8
Millenmark <millenmark.iv@gmail.com>
2023-04-27 12:42:33 +0800

    added index.js and .env

--- server/index.js
+++ server/index.js
@@ -1,19 +1,32 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoute = require("./routes/userRoute")
 const app = express();
+require("dotenv").config()
 
-const port = 3001;
+//When using 'use', it is a middleware
+app.use(express.json());
+app.use(cors());
+app.use("/api/users", userRoute)
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// CRUD OPERATIONS END POINT
+app.get("/", (req, res) => {
+  res.send("Welcome to our chat app :D");
 });
 
-const server = app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+const PORT = process.env.PORT || 5000;
+const URI = process.env.ATLAS_URI;
 
-// Check if server is already running
-if (server.listening) {
-  console.log(`Server is already running on port ${port}`);
-} else {
-  console.log(`Server is not running`);
-}
+app.listen(PORT, (req, res) => {
+  console.log(`server running on port: ${PORT}`);
+})
+
+mongoose.connect(URI,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("Successfully Connected to MongoDB ATLAS");
+}).catch((error) => {
+  console.log("MongoDB Connection Failed!", error.message);
+});
