aea56e9ae4ae41d13bc7cd9331819184177c8fcb
Millenmark <millenmark.iv@gmail.com>
2023-04-28 14:44:22 +0800

    updates

--- client/src/components/UserChat/UserChat.jsx
+++ client/src/components/UserChat/UserChat.jsx
@@ -1,6 +1,25 @@
+import style from "./UserChat.module.css"
+import { useFetchRecipientUser } from "../../hooks/useFetchRecipient"
+import avatarMale from "../../assets/avatar_male.svg"
+
 const UserChat = ({chat, user}) => {
+  const {recipientUser} = useFetchRecipientUser(chat, user)
+
   return (
-    <div>UserChat</div>
+    <a href="#" className={style.userChatsWrapper}>
+      <div className={style.userRecipient}>
+        <img src={avatarMale} alt="" />
+        <div className={style.details}>
+          <h4 style={{fontWeight: "600"}}>{recipientUser?.name}</h4>
+          <p>Messages shows here</p>
+        </div>
+      </div>
+
+      <div className={style.dateWrapper}>
+        <div className={style.date}>12/12/2022</div>
+        <div className={style.notification}></div>
+      </div>
+    </a>
   )
 }
 
